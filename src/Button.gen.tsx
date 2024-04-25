1f783cf1afe9af7f8445384559ceee6c2a5f506e
Pained Pineapple <github.com.nineteen128@passmail.net>
2024-04-25 08:57:13 -0700

    init

---
+++ src/Button.gen.tsx
@@ -0,0 +1,8 @@
+/* TypeScript file generated from Button.res by genType. */
+
+/* eslint-disable */
+/* tslint:disable */
+
+import type {t as Exn_t} from './Exn.gen.tsx';
+
+export type test = { readonly name: Exn_t };
